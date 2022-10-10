fe3a45d68fb8f0ec1260ac032f22047878779fbd
FedericoMagini <federico.magini@example.com>
2022-10-10 14:55:32 +0200

    java backend

---
+++ BackEnd/JavaBackEnd/src/main/java/it/unikey/javabackend/JavaBackEndApplication.java
@@ -0,0 +1,13 @@
+package it.unikey.javabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackEndApplication.class, args);
+	}
+
+}
---
+++ BackEnd/JavaBackEnd/src/test/java/it/unikey/javabackend/JavaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unikey.javabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
